82d6a363c26ceb8b38b0dbb6b4c005f6a410f2a2
Jerry Sewell <ellesterate@gmail.com>
2014-11-18 11:23:18 +0300

    fix

--- Informatics/1790/main.cpp
+++ Informatics/1790/main.cpp
@@ -17,43 +17,36 @@ int main() {
     //freopen("output.txt", "w", stdout);
     int n, m;
     cin >> n;
-    vector<int> a(n+10);
+    vector<int> a(n+1);
     vector<int> b;
     for(int i = 1; i <= n; i++)
         scanf("%ld", &a[i]);
     cin >> m;
-    b.resize(m+10);
+    b.resize(m+1);
     for(int i = 1; i <= m; i++)
         scanf("%ld", &b[i]);
 
     vector<vector<int> > g(n+1, vector<int>(m+1, 0));
     for(int i = 1; i <= n; i++)
         for(int j = 1; j <= m; j++)
-            g[i][j] = (a[i] == b[j]) ? (g[i-1][j-1] + 1) : 0;
+            g[i][j] = (a[i] == b[j]) ? (g[i-1][j-1] + 1) : (max(g[i-1][j], g[i][j-1]));
     mx d;
     d.k = 0;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++)
-            if(d.k < g[i][j])
-            {
-                d.k = g[i][j];
-                d.x = i;
-                d.y = j;
-            }
-    }
+    d.k = g[n][m];
+    d.x = n;
+    d.y = m;
     if(d.k == 0)
     {
         exit(EXIT_SUCCESS);
     }
     vector<int> way;
-    while(d.x <= n && d.x >= 1 && d.y <= m && d.y >= 1 && g[d.x][d.y] > 0)
+    for(;g[d.x][d.y] != 0; d.x--, d.y--)
     {
+        for(; d.y > 1 && g[d.x][d.y] == g[d.x][d.y-1]; d.y--);
+        for(; d.x > 1 && g[d.x][d.y] == g[d.x-1][d.y]; d.x--);
         way.push_back(a[d.x]);
-        if(g[d.x][d.y] - 1 != g[d.x-1][d.y-1])
-            break;
-        d.x--; d.y--;
     }
-    for(int i = way.size() - 1; i >= 0; i--)
+    for(int i = way.size()-1; i >= 0; i--)
         printf("%ld ", way[i]);
     cout << endl;
     return 0;
