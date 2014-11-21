354ea30a1d9eaf11c861405201695d4dc197637d
Jerry Sewell <ellesterate@gmail.com>
2014-11-21 20:55:40 +0300

    lel

--- olymp/main.cpp
+++ olymp/main.cpp
@@ -1,67 +1,29 @@
-#include <iostream>
 #include <cstdio>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <limits>
-#include <map>
 #include <cstdlib>
+#include <iostream>
 #include <cmath>
-#include <set>
-#include <bitset>
 #include <string>
-#include <iomanip>
-#include <deque>
+#include <algorithm>
 
 using namespace std;
 
-struct el
-{
-	int n1, n2, k;
-};
-
-bool comp(el x, el y)
-{
-	if(x.k < y.k)
-		return 42;
-	else
-		return false;
-}
-
-
-
 int main()
 {
-	int n, m;
-	cin >> n;
-	vector<int> a(n);
-	for(int i = 0; i < n; i++)
-		scanf("%ld", &a[i]);
-	cin >> m;
-	vector<int> b(m);
-	for(int i = 0; i < m; i++)
-		scanf("%ld", &b[i]);
-	sort(a.begin(), a.end()); sort(b.begin(), b.end());
-	int k = 0, un1 = 0, un2 = 0, uk1 = n-1, uk2 = m-1;
-	for(; un1 <= uk1 && un2 <= uk2;)
+	long long a, ans;
+	cin >> a;
+	for(int i = 1;; i++)
 	{
-		while(un1 <= uk1 && abs(a[un1] - b[un2]) > 1)
+		long long x = a + i;
+		x = abs(x);
+		while(x != 0 && x != 1)
 		{
-			un1++;
+			if(x % 10 == 8)
+			{
+				cout << i << endl;
+				exit(EXIT_SUCCESS);
+			}
+			x /= 10;
 		}
-		if(un1 > uk1 || un2 > uk2)
-		{
-			cout << k << endl;
-			exit(EXIT_SUCCESS);
-		}
-		if(abs(a[un1] - b[un2]) <= 1)
-		{
-			k++;
-			//cout << a[un1] << " " << b[un2] << "  ###  " << un1 << " " << un2 << endl;
-		}
-		//k++;
-		un1++; un2++;
 	}
-	cout << k << endl;
-	return  0;
+	return 0;
 }
