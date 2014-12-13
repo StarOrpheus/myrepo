c3c5c3542d7c559156718021b5859161eac7099b
Jerry Sewell <ellesterate@gmail.com>
2014-12-13 20:18:12 +0300

    lel

--- olymp/main.cpp
+++ olymp/main.cpp
@@ -9,38 +9,31 @@
 using namespace std;
 
 int main(){
-	int n, l;
-	cin >> n >> l;
-	vector<int> a(n+1);
-	for(int i = 1; i <= n; i++)
-		//scanf("%ld", &a[i]);
-		cin >> a[i];
-	sort(a.begin()+1, a.end());
-	//a[0] = 0;
-	int max_l = -1;
-	for(int i = 2; i <= n; i++)
+	int a, b;
+	cin >> a >> b;
+	if(a == b)
 	{
-		if(a[i]-a[i-1] > max_l)
+		cout << "infinity\n";
+		exit(EXIT_SUCCESS);
+	}
+	int t = a - b;
+	int k = 0;
+	int d = 2;
+	while(d * d < t)
+	{
+		if(t % d == 0)
 		{
-			max_l = a[i] - a[i-1];
-			//cout << a[i] << " " << a[i-1] << endl;
+			if(d > b)
+				k++;
+			if(t / d > b)
+				k++;
 		}
+		d++;
 	}
-	if(n == 1)
-	{
-		printf("%.9f\n" , (double) max(a[1], l - a[1]));
-		exit(EXIT_SUCCESS);
-	}
-	if(a[1] > max_l / 2.0 || l - a[n] > max_l / 2.0)
-	{
-		if(a[1] > l - a[n])
-			printf("%.9f \n", (double) a[1]);
-		else
-			printf("%.9f \n", (double) l-a[n]);
-		exit(EXIT_SUCCESS);
-	}
-
-	//cout << a[3] << " " << a[5] << endl;
-	printf("%.9f\n", max_l / 2.0000);
+	if(t > b)
+		k++;
+	if(d > b && t % d == 0)
+		k++;
+	cout << k << endl;
 	return 0;
 }
