1bf6717ce6760f9469fc5bc9d60d47a067fcff4d
Jerry Sewell <ellesterate@gmail.com>
2014-12-27 23:03:49 +0300

    lel

--- main.cpp
+++ main.cpp
@@ -5,67 +5,21 @@
 #include <cstdio>
 #include <stack>
 #include <algorithm>
-#include <cstring>
-
-#define MIN(a, b) ((a < b) ? (a) : (b))
-#define MAX(a, b) ((a > b) ? (a) : (b))
-
+#include <limits>
+#include <list>
+#include <queue>
+#include <iomanip>
 using namespace std;
 
-struct interv{
-    int open;
-    int close;
-    interv(){}
-    interv(int x, int y)
-    {
-        open = x;
-        open = y;
-    }
-};
-struct st_el{
-    int el;
-    char type;
-};
-
-bool inline comp(st_el a, st_el b)
+template<typename a>
+void lel(a kawai)
 {
-    if(a.el < b.el)
-        return true;
-    if(a.el > b.el)
-        return false;
-    if(!a.type && b.type)
-        return true;
-    return false;
+    cout << kawai << endl;
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    vector<interv> a(n);
-    int t;
-    for(int i = 0; i < n; i++)
-    {
-        cin >> a[i].open >> t;
-        a[i].close = a[i].open + t;
-    }
-    vector<st_el> st(2 * n);
-    for(int i = 0; i < n; i++)
-    {
-        st[i].el        = a[i].open;
-        st[i].type      = 0b1;
-        st[n + i].el    = a[i].close;
-        st[n+i].type    = 0b0;
-    }
-    sort(st.begin(), st.end(), comp);
-    //for(int i = 0; i < 2 * n; i++)
-        //cout << (int) st[i].el << " # " << (int) st[i].type << endl;
-    int k_open = 0, k_m = 0;
-    for(int i = 0; i < 2 * n; i++)
-    {
-        k_open += (st[i].type) ? 1 : -1;
-        k_m = MAX(k_open, k_m);
-    }
-    cout << k_m << endl;
+    lel(7);
+    lel("Nyaa\n");
     return 0;
 }
