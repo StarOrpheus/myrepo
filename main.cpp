396c0b70a9088c15433f12926de204de345b1f77
Jerry Sewell <ellesterate@gmail.com>
2014-12-30 12:40:21 +0300

    lel

--- main.cpp
+++ main.cpp
@@ -5,21 +5,62 @@
 #include <cstdio>
 #include <stack>
 #include <algorithm>
+#include <fstream>
 #include <limits>
 #include <list>
 #include <queue>
 #include <iomanip>
-using namespace std;
+#include <cpufreq.h>
+#include <bitset>
 
-template<typename a>
-void lel(a kawai)
+using namespace std;
+#define vecotr vector
+
+vector<bool> used;
+vector<vector<int> > g;
+vector<int> mt;
+
+bool dfs(int v)
 {
-    cout << kawai << endl;
+    if(used[v])
+        return false;
+    used[v] = true;
+    for(int i = 0; i < g[v].size(); i++)
+    {
+        int to = g[v][i];
+        if(mt[to] == -1 || dfs(mt[to]))
+        {
+            mt[to] = v;
+            return true;
+        }
+    }
+    return false;
 }
 
 int main()
 {
-    lel(7);
-    lel("Nyaa\n");
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    int n, k, t1, t2;
+    cin >> n >> k;
+    g.resize(n+1);
+    for(int i = 1; i <= k*n; i++)
+    {
+        cin >> t1 >> t2;
+        g[t1].push_back(t2);
+    }
+    used.resize(n+1);
+    mt.resize(n+1, -1);
+    for(int i = 1; i <= n; i++)
+    {
+        used.assign(n+1, false);
+        dfs(i);
+    }
+    for(int i = 1; i <= n; i++)
+    {
+        if(!(mt[i] == -1))
+        {
+            cout << mt[i] << " " << i << endl;
+        }
+    }
     return 0;
 }
