a124ec0a9073df07ac27823ff764f5cb092235ca
Jerry Sewell <ellesterate@gmail.com>
2015-01-10 16:01:29 +0300

    olymp

--- olymp/main.cpp
+++ olymp/main.cpp
@@ -1,79 +1,56 @@
+/*
+    Author      - Andrew[QJEX] Berlin
+    Created     - 10.01.2015 15:50
+ */
+
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
-#include <iomanip>
-#include <cmath>
 #include <string>
-#include <vector>
-#include <bitset>
-#include <deque>
-#include <memory>
-#include <algorithm>
+#include <cstring>
+#include <cmath>
+
+
 using namespace std;
 
+long long a[10050];
+long long b[10050];
 
-#define MIN(a, b) ((a < b) ? (a) : (b))
-#define MINN(a, b, c) (MIN(a, MIN(b, c)))
-#define MAX(a, b) ((a > b) ? (a) : (b))
-#define MAXX(a, b, c) (MAX(a, MAX(b, c)))
+#define rsqr(x) ((x)*(x))
 
-int main()
-{
-    const long long INF = 1000*1000*1000 + 7;
-    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
-    int c[10020];
-    char t;
-    int n = 0;
-    while((t = getchar()) != EOF && t != '\n')
-    {
-        c[++n] = t - '0';
+int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int l,w,n;
+    long long ans = 0;
+    cin >> l >> w;
+    cin >> n;
+    for(int i=1; i<= n; i++){
+        cin >> a[i];
     }
-    vector<vector<vector<vector<long long > > > > f(n+12, vector<vector<vector<long long > > >(11, vector<vector<long long > >(11, vector<long long>(3, 0))));
-    for(int x = 0; x <= 9; x++)
-        for(int y = 0; y < 10; y++)
-            for(int p = 0; p <= 1; p++)
-                if(x + y == (10 * p) + c[n])
-                    f[n][x][y][p] = 1;
+    int m;
+    cin >> m;
+    for(int i =1; i<=m; i++){
+        cin >> b[i];
+    }
+    long long w1 = rsqr(w);
+    for (int ii1 = 1; ii1<= n ; ii1++ ){
+        for (int ii2 = ii1; ii2<= n ; ii2++ ){
+            for (int jj1 = 1; jj1<= m ; jj1++ ){
+                for (int jj2 = jj1; jj2<= m ; jj2++ ){
+                    long long d = (a[ii2]-a[ii1])+(b[jj2]-b[jj1])+sqrt((double)w1+rsqr(a[ii1]-b[jj1]))+sqrt((double)w1+rsqr(a[ii2]-b[jj2]));
+                    if (d <= l){
+                        if (ans <  ii2-ii1+jj2-jj1 +2){
+                            ans = ii2-ii1+jj2-jj1 +2;
+                        }
 
-    for(int k = n-1; k >= 1; k--)
-    {
-        for(int i = 0; i <= 9; i++)
-            for(int j = 0; j <= 9; j++)
-                for(int p = 0; p <= 1; p++)
-                {
-                    if(c[k] + (10 * p) == i + j)
-                    {
-                        long long ans = 0;
-                        for(int x = 0; x <= 9; x++)
-                            for(int y = 0; y <= 9; y++)
-                                if(x != i && y != j)
-                                {
-                                    ans = (ans + f[k+1][x][y][0]) % INF;
-                                }
-                        f[k][i][j][p] = ans;
-                    } else if(c[k] + (10 * p) - 1 == i + j)
-                    {
-                        long long ans = 0;
-                        for(int x = 0; x <= 9; x++)
-                            for(int y = 0; y <= 9; y++)
-                                if(x != i && y != j)
-                                {
-                                    ans = (ans + f[k+1][x][y][1]) % INF;
-                                }
-                        f[k][i][j][p] = ans;
-                    } else {
-                        f[k][i][j][p] = 0;
+
                     }
                 }
-    }
 
-    long long ans = 0;
-    for(int x = 1; x <= 9; x++)
-    {
-        for(int y = 1; y <= 9; y++)
-            for(int p = 0; p <= 0; p++)
-               ans = (ans +f[1][x][y][p]) % INF;
+            }
+        }
     }
-    cout << ans << endl;
+    cout << ans;
     return 0;
 }
