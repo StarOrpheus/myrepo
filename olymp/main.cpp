4b0c951390e0c9f35072340a84a6200669d2a9f7
Jerry Sewell <ellesterate@gmail.com>
2015-01-10 15:16:05 +0300

    olymp

--- olymp/main.cpp
+++ olymp/main.cpp
@@ -1,60 +1,79 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <cstdlib>
 #include <cstdio>
-#include <stack>
-#include <algorithm>
-#include <limits>
-#include <list>
-#include <queue>
 #include <iomanip>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <deque>
+#include <memory>
+#include <algorithm>
 using namespace std;
 
 
+#define MIN(a, b) ((a < b) ? (a) : (b))
+#define MINN(a, b, c) (MIN(a, MIN(b, c)))
+#define MAX(a, b) ((a > b) ? (a) : (b))
+#define MAXX(a, b, c) (MAX(a, MAX(b, c)))
+
 int main()
 {
-    int n, m;
-    cout << fixed << setprecision(2);
-    cin >> n >> m;
-    vector<vector<double> > a(n+1, vector<double>(m+1, 0));
-    int t1, t2, t3;
-    for(int i = 1; i <= n; i++)
+    const long long INF = 1000*1000*1000 + 7;
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    int c[10020];
+    char t;
+    int n = 0;
+    while((t = getchar()) != EOF && t != '\n')
     {
-        cin >> t1 >> t2;
-        a[i][t1] = t2;
+        c[++n] = t - '0';
     }
-    int nn;
-    cin >> nn;
-    for(int i = 1; i <= nn; i++)
+    vector<vector<vector<vector<long long > > > > f(n+12, vector<vector<vector<long long > > >(11, vector<vector<long long > >(11, vector<long long>(3, 0))));
+    for(int x = 0; x <= 9; x++)
+        for(int y = 0; y < 10; y++)
+            for(int p = 0; p <= 1; p++)
+                if(x + y == (10 * p) + c[n])
+                    f[n][x][y][p] = 1;
+
+    for(int k = n-1; k >= 1; k--)
     {
-        cin >> t1 >> t2 >> t3;
-        for(int j = 1; j <= m; j++)
-        {
-            if(a[t2][j] < 0.0000000001)
-                a[t2][j] = 0;
-            if(a[t1][j] < 0.0000000001)
-                a[t1][j] = 0;
-            a[t2][j] += a[t1][j] / 100.0 * t3;
-            a[t1][j] -= a[t1][j] / 100.0 * t3;
-            if(a[t2][j] < 0.0000000001)
-                a[t2][j] = 0;
-            if(a[t1][j] < 0.0000000001)
-                a[t1][j] = 0;
-        }
+        for(int i = 0; i <= 9; i++)
+            for(int j = 0; j <= 9; j++)
+                for(int p = 0; p <= 1; p++)
+                {
+                    if(c[k] + (10 * p) == i + j)
+                    {
+                        long long ans = 0;
+                        for(int x = 0; x <= 9; x++)
+                            for(int y = 0; y <= 9; y++)
+                                if(x != i && y != j)
+                                {
+                                    ans = (ans + f[k+1][x][y][0]) % INF;
+                                }
+                        f[k][i][j][p] = ans;
+                    } else if(c[k] + (10 * p) - 1 == i + j)
+                    {
+                        long long ans = 0;
+                        for(int x = 0; x <= 9; x++)
+                            for(int y = 0; y <= 9; y++)
+                                if(x != i && y != j)
+                                {
+                                    ans = (ans + f[k+1][x][y][1]) % INF;
+                                }
+                        f[k][i][j][p] = ans;
+                    } else {
+                        f[k][i][j][p] = 0;
+                    }
+                }
     }
-    long double all = 0;
-    for(int i = 1; i <= m; i++)
-        all += a[1][i];
-    for(int i = 1; i <= m; i++)
+
+    long long ans = 0;
+    for(int x = 1; x <= 9; x++)
     {
-        if(a[1][i] < 0.0000000001)
-            a[1][i] = 0;
-        //if(a[1][i])
-        //{
-            cout << (a[1][i] / all * 100) << " ";
-        //}
+        for(int y = 1; y <= 9; y++)
+            for(int p = 0; p <= 0; p++)
+               ans = (ans +f[1][x][y][p]) % INF;
     }
-    cout << endl;
+    cout << ans << endl;
     return 0;
 }
