54ad953808aaa150178b765c5b238f5a56a7d1e1
Jerry Sewell <ellesterate@gmail.com>
2015-01-28 21:21:49 +0300

    pechal

--- 2784.cpp
+++ 2784.cpp
@@ -25,7 +25,8 @@ struct __attribute__((packed)) edge
     edge(int y, int z, int f, int p) : y(y), z(z), f(f), p(p) {}
 };
 
-vector<bool> d;
+//vector<bool> d;
+bool d[100000];
 int n, m;
 vector<vector<edge > > g;
 
@@ -34,21 +35,29 @@ inline bool bfs()
     for(int i = 1; i <= n; i++)
         d[i] = -1;
     d[1] = 0;
+//    for(int i = 1; i <= n; i++)
+//        printf("%ld ", d[i]);
+//    putchar('\n');
     queue<int> q;
     q.push(1);
-    while(!q.empty())
+    while(!q.empty() && d[n] == -1)
     {
         int v = q.front(); q.pop();
         for(int i = 0; i < g[v].size(); i++)
         {
-            if(g[v][i].z == -1 && g[v][i].f < g[v][i].z)
+            if(d[g[v][i].y] == -1 && g[v][i].f < g[v][i].z)
             {
                 d[g[v][i].y] = d[v] + 1;
                 q.push(g[v][i].y);
             }
         }
     }
-    printf("hmmm...\n");
+//    for(int i = 1; i <= n; i++)
+//        printf("%ld ", d[i]);
+//    putchar('\n');
+//    putchar('\n');
+//    putchar('\n');
+    //printf("hmmm...\n");
     return (d[n] != -1);
 }
 
@@ -79,7 +88,7 @@ int main() {
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
     cin >> n >> m;
-    d.resize(n+1);
+//    d.resize(n+1, -1);
     int x,y,z;
     g.resize(n+1);
     for(int i = 0; i < m; i++)
@@ -92,11 +101,11 @@ int main() {
     int pot = 0;
     while(bfs())
     {
-        for(int i = 1; i <= n; i++)
-            printf("%ld ", d[i]);
+//        for(int i = 1; i <= n; i++)
+//            printf("%ld ", d[i]);
         while(int pu = dfs(1, INF))
             pot += pu;
-
+//        break;
     }
     cout << pot << endl;
     return 0;
