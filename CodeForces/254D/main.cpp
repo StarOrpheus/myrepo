caa269338af0bc85e677f57ecafa618a0f084bd5
Jerry Sewell <ellesterate@gmail.com>
2015-02-03 12:56:46 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- CodeForces/254D/main.cpp
+++ CodeForces/254D/main.cpp
@@ -2,7 +2,6 @@
 #include <cstdio>
 #include <algorithm>
 #include <cstdlib>
-#include <cstring>
 #include <string>
 #include <vector>
 #include <map>
@@ -46,6 +45,7 @@ cond get_cond(char c)
 
 int main()
 {
+//    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
     int n, m, d;
     ios_base::sync_with_stdio(0);
     cin >> n >> m >> d;
@@ -65,16 +65,19 @@ int main()
     }
     int i = 1, j = 1;
     bool fl = false;
-    for(; i <= n && !fl; i++)
+    for(i = 1; i <= n && !fl; i++)
         for(j = 1; j <= m && !fl; j++)
             if(a[i][j] == RAT)
                 fl = true;
+    //cout << i << " " << j << endl;
+    i--; j--;
     vector<vector<int > > f(n+1, vector<int>(m+1, -1));
     queue<coord> q;
+    f[i][j] = 0;
     q.push(coord(i, j));
     int rats_on_heaven = 0;
-    
-    while(!q.empty() && rats_on_heaven < rat_count)
+
+    while(!q.empty() && rats_on_heaven <= rat_count)
     {
         coord temp = q.front();
         q.pop();
@@ -82,24 +85,57 @@ int main()
             rats_on_heaven++;
         for(int nap = 0; nap < 4; nap++)
         {
-            #define NEWX (temp.x + dx[nap])
-            #define NEWY (temp.y + dy[nap])
-            if(NEWX > 0 && NEWX < n+1 && NEWY > 0 && NEWY < m+1 && f[NEWX][NEWY] == -1)
+            #define NEW_X (temp.x + dx[nap])
+            #define NEW_Y (temp.y + dy[nap])
+            if(NEW_X > 0 && NEW_X < n+1 && NEW_Y > 0 && NEW_Y < m+1 && f[NEW_X][NEW_Y] == -1 && a[NEW_X][NEW_Y] != WALL)
             {
-                f[NEWX][NEWY] = f[temp.x][temp.y] + 1;
-                q.push(coord(NEWX, NEWY ) );
+                if(f[temp.x][temp.y] == d)
+                    break;
+                f[NEW_X][NEW_Y] = f[temp.x][temp.y] + 1;
+                q.push(coord(NEW_X, NEW_Y ) );
             }
-            #undef NEWX
-            #undef NEWY
+            #undef NEW_X
+            #undef NEW_Y
         }
     }
+
     for(int i = 1; i <= n; i++)
-    {
         for(int j = 1; j <= m; j++)
         {
-            cout << f[i][j] << " ";
+            if(f[i][j] != -1)
+            {
+                vector<vector<int > > f(n+1, vector<int>(m+1, -1));
+                queue<coord> q;
+                f[i][j] = 0;
+                q.push(coord(i, j));
+                int rats_on_heaven = 0;
+
+                while(!q.empty() && rats_on_heaven <= rat_count)
+                {
+                    coord temp = q.front();
+                    q.pop();
+                    if(a[temp.x][temp.y] == RAT)
+                        rats_on_heaven++;
+                    for(int nap = 0; nap < 4; nap++)
+                    {
+#define NEW_X (temp.x + dx[nap])
+#define NEW_Y (temp.y + dy[nap])
+                        if(NEW_X > 0 && NEW_X < n+1 && NEW_Y > 0 && NEW_Y < m+1 && f[NEW_X][NEW_Y] == -1 && a[NEW_X][NEW_Y] != WALL)
+                        {
+                            if(f[temp.x][temp.y] == d)
+                                break;
+                            f[NEW_X][NEW_Y] = f[temp.x][temp.y] + 1;
+                            q.push(coord(NEW_X, NEW_Y ) );
+                        }
+#undef NEW_X
+#undef NEW_Y
+                    }
+                }
+
+            }
         }
-        cout << endl;
-    }
+
+
+
     return 0;
 }
