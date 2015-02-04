bc96558d1f6e48d10bc5ddaf02f6adb175251be7
Jerry Sewell <ellesterate@gmail.com>
2015-02-04 16:13:58 +0300

    Hell yeah!

--- CodeForces/254D/main.cpp
+++ CodeForces/254D/main.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <cmath>
 #include <map>
 #include <set>
 #include <iomanip>
@@ -15,127 +16,88 @@ using namespace std;
 
 #define INF (1<<30)
 
-struct coord
-{
-    int x;
-    int y;
-    coord(){}
-    coord(int x, int y) : x(x), y(y) {}
-};
+#define SQR(x)                  ((x)*(x)*1LL)
+//#define RANGE(x1, y1, x2, y2)   (sqrt(SQR(x1-x2) + SQR(y1-y2)))
 
-const int dx[] = {-1, 0, 1, 0};
-const int dy[] = { 0, 1, 0,-1};
-
-enum cond
+long double RANGE(long double x1, long double y1, long double x2, long double y2)
 {
-    WALL    = -1,
-    RAT     =  1,
-    EMPTY   =  0
-};
-
-cond get_cond(char c)
-{
-    if(c == 'X')
-        return WALL;
-    if(c == '.')
-        return EMPTY;
-    if(c == 'R')
-        return RAT;
+    return sqrt(SQR(x1-x2) + SQR(y1-y2));
 }
+
+struct point
+{
+    int             ind;
+    long double     rast;
+    long double     x;
+    long double     y;
+    point(){}
+    point(int ind, long double rast, long double x, long double y) : ind(ind), rast(rast), x(x), y(y) {}
+};
+
 
 int main()
 {
 //    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
-    int n, m, d;
-    ios_base::sync_with_stdio(0);
-    cin >> n >> m >> d;
-    vector<vector<cond > > a(n+1, vector<cond >(m+1));
-    int c;
-    int rat_count = 0;
-    string ts;
-    for(int i = 1; i <= n; i++)
+    int n, m;
+    cin >> n >> m;
+    long double a, b;
+    cin >> a >> b;
+    vector<point> z_b(n);
+    vector<point> v_b(m);
+//    point *z_b = (point *) malloc(sizeof(point) * n);
+//    point *v_b = (point *) malloc(sizeof(point) * m);
+    for(int i = 0; i < n; i++)
     {
-        cin >> ts;
-        for(int j = 0; j < m; j++)
-        {
-            a[i][j+1] = get_cond(ts[j]);
-            if(a[i][j+1] == RAT)
-                rat_count++;
-        }
+        int y;
+//        scanf("%ld", &y);
+        cin >> y;
+        z_b[i].ind      = i+1;
+        z_b[i].rast     = RANGE(0, 0, a, y);
+        z_b[i].x        = a;
+        z_b[i].y        = y;
     }
-    int i = 1, j = 1;
-    bool fl = false;
-    for(i = 1; i <= n && !fl; i++)
-        for(j = 1; j <= m && !fl; j++)
-            if(a[i][j] == RAT)
-                fl = true;
-    //cout << i << " " << j << endl;
-    i--; j--;
-    vector<vector<int > > f(n+1, vector<int>(m+1, -1));
-    queue<coord> q;
-    f[i][j] = 0;
-    q.push(coord(i, j));
-    int rats_on_heaven = 0;
-
-    while(!q.empty() && rats_on_heaven <= rat_count)
+    for(int i = 0; i < m; i++)
     {
-        coord temp = q.front();
-        q.pop();
-        if(a[temp.x][temp.y] == RAT)
-            rats_on_heaven++;
-        for(int nap = 0; nap < 4; nap++)
+        int y;
+//        scanf("%ld", &y);
+        cin >> y;
+        v_b[i].ind      = i+1;
+        v_b[i].x        = b;
+        v_b[i].y        = y;
+    }
+    for(int i = 0; i < m; i++)
+    {
+        int r;
+        cin >> r;
+        v_b[i].rast     = r;
+    }
+    int b_iz = 0, b_iv = 0;
+    for(int i = 0; i < m; i++)
+    {
+        int l = 0, r = n-1;
+        while(r - l >= 3)
         {
-            #define NEW_X (temp.x + dx[nap])
-            #define NEW_Y (temp.y + dy[nap])
-            if(NEW_X > 0 && NEW_X < n+1 && NEW_Y > 0 && NEW_Y < m+1 && f[NEW_X][NEW_Y] == -1 && a[NEW_X][NEW_Y] != WALL)
+            int ms1 = l + (r - l) / 3, ms2 = r - (r - l) / 3;
+            if(z_b[ms1].rast + v_b[i].rast + RANGE(z_b[ms1].x, z_b[ms1].y, v_b[i].x, v_b[i].y) >/*!!!!*/ z_b[ms2].rast + v_b[i].rast + RANGE(z_b[ms2].x, z_b[ms2].y, v_b[i].x, v_b[i].y))
             {
-                if(f[temp.x][temp.y] == d)
-                    break;
-                f[NEW_X][NEW_Y] = f[temp.x][temp.y] + 1;
-                q.push(coord(NEW_X, NEW_Y ) );
+                l = ms1 + 1;
+            } else
+            {
+                r = ms2 - 1;
             }
-            #undef NEW_X
-            #undef NEW_Y
+//            cout << l << " "  << r << endl;
         }
-    }
-
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= m; j++)
+        for(int it = max(0, l-1); it <= min(r+1, n-1); it++)
         {
-            if(f[i][j] != -1)
+            if(z_b[b_iz].rast + RANGE(z_b[b_iz].x, z_b[b_iz].y, v_b[b_iv].x, v_b[b_iv].y) + v_b[b_iv].rast > z_b[it].rast + RANGE(z_b[it].x, z_b[it].y, v_b[i].x, v_b[i].y) + v_b[i].rast)
             {
-                vector<vector<int > > f(n+1, vector<int>(m+1, -1));
-                queue<coord> q;
-                f[i][j] = 0;
-                q.push(coord(i, j));
-                int rats_on_heaven = 0;
-
-                while(!q.empty() && rats_on_heaven <= rat_count)
-                {
-                    coord temp = q.front();
-                    q.pop();
-                    if(a[temp.x][temp.y] == RAT)
-                        rats_on_heaven++;
-                    for(int nap = 0; nap < 4; nap++)
-                    {
-#define NEW_X (temp.x + dx[nap])
-#define NEW_Y (temp.y + dy[nap])
-                        if(NEW_X > 0 && NEW_X < n+1 && NEW_Y > 0 && NEW_Y < m+1 && f[NEW_X][NEW_Y] == -1 && a[NEW_X][NEW_Y] != WALL)
-                        {
-                            if(f[temp.x][temp.y] == d)
-                                break;
-                            f[NEW_X][NEW_Y] = f[temp.x][temp.y] + 1;
-                            q.push(coord(NEW_X, NEW_Y ) );
-                        }
-#undef NEW_X
-#undef NEW_Y
-                    }
-                }
-
+                b_iz = it;
+                b_iv = i;
             }
         }
-
-
-
+    }
+//    free(z_b);
+//    free(v_b);
+    cout << (b_iz+1) << " " << (b_iv+1) << endl;
     return 0;
 }
