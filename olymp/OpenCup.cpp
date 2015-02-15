b390ffccb4b7d0b3e43d48928b6431f2bd0d3a3b
Jerry Sewell <ellesterate@gmail.com>
2015-02-15 13:27:18 +0300

    On work

---
+++ olymp/OpenCup.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <limits>
+#include <iomanip>
+
+using namespace std;
+
+#define MIN(a, b) ((a < b) ? (a) : (b))
+#define MAX(a, b) ((a > b) ? (a) : (b))
+
+int main(){
+//    freopen("maze.in", "r", stdin);
+    int k_t;
+    cin >> k_t;
+    for(; k_t; k_t--)
+    {
+        string s;
+        cin >> s;
+        
+    }
+    return 0;
+}
