c84b5c04a80d12fb51146317f3611392761f62f2
Jerry Sewell <ellesterate@gmail.com>
2015-02-16 15:00:04 +0300

    On work

--- main.cpp
+++ main.cpp
@@ -53,21 +53,27 @@ int main(){
                 if(ia + ib + ic >= k)
                     break;
                 int id = k - ia - ib - ic;
-                if(ia != ib != ic != id)
+                if(ia + ib + ic + id != k)
+                    break;
+                if(ia != ib && ia != ic && ia != id && ib != ic && ib != id && ic != id)
                 {
                     ans += a[ia]*a[ib]*a[ic]*a[id];
+//                    printf("%ld %ld %ld %ld\n", ia, ib, ic, id);
                     continue;
-                } else if(ia == ib && ia != ic && ia != id)
+                } else if(ia == ib && ia != ic && ia != id && a[ia] >= 2)
                 {
                     ans += a[ia] * (a[ia] - 1) / 2 * a[ic] * a[id];
+//                    printf("%ld %ld %ld\n", ia, ic, id);
                     continue;
-                } else if(ia == ib == ic && ic != id)
+                } else if(ia == ib && ib == ic && ic != id && a[ia] >= 3)
                 {
                     ans += (a[ia]*(a[ia] - 1)*(a[ia] - 2)) / fac(3) * a[id];
+//                    printf("%ld %ld\n", ia, id);
                     continue;
-                } else
+                } else if(a[ia] >= 4)
                 {
                     ans += (a[ia]*(a[ia] - 1) * (a[ia] - 2) * (a[ia] - 3)) / fac(4);
+//                    printf("%ld\n", ia);
                     continue;
                 }
             }
