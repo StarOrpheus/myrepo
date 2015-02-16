47f32fbaac4cd41168510a4b9b9429caac75d7f7
Jerry Sewell <ellesterate@gmail.com>
2015-02-16 15:34:41 +0300

    On work

--- main.cpp
+++ main.cpp
@@ -16,6 +16,11 @@ using namespace std;
 #define MIN(a, b) ((a < b) ? (a) : (b))
 #define MAX(a, b) ((a > b) ? (a) : (b))
 
+int inline hsh(int a, int b, int c, int d)
+{
+    return a + 2*b + 4 * c + 8 * d;
+}
+
 int fac(int c)
 {
     if(c == 1)
@@ -27,6 +32,8 @@ int fac(int c)
 
 int main(){
 //    freopen("lel.out", "w", stdout);
+    bool us[1510];
+    memset((void *) us, '\0', sizeof(bool) * 1510);
     int k;
     int n;
     cin >> k;
@@ -46,39 +53,43 @@ int main(){
     }
     for(int ia = 0; ia <= 100; ia++)
     {
-        for(int ib = 0; ib <= 100; ib++)
+        for(int ib = ia; ib <= 100; ib++)
         {
-            for(int ic = 0; ic <= 100; ic++)
+            for(int ic = ib; ic <= 100; ic++)
             {
                 if(ia + ib + ic >= k)
                     break;
                 int id = k - ia - ib - ic;
                 if(ia + ib + ic + id != k)
                     break;
-                if(ia != ib && ia != ic && ia != id && ib != ic && ib != id && ic != id)
+                if(ia != ib && ia != ic && ia != id && ib != ic && ib != id && ic != id && !us[hsh(ia, ib, ic, id)])
                 {
                     ans += a[ia]*a[ib]*a[ic]*a[id];
+                    us[hsh(ia, ib, ic, id)] = 42;
 //                    printf("%ld %ld %ld %ld\n", ia, ib, ic, id);
                     continue;
-                } else if(ia == ib && ia != ic && ia != id && a[ia] >= 2)
+                } else if(ia == ib && ia != ic && ia != id && a[ia] >= 2 && !us[hsh(ia, ib, ic, id)])
                 {
                     ans += a[ia] * (a[ia] - 1) / 2 * a[ic] * a[id];
+                    us[hsh(ia, ib, ic, id)] = 42;
 //                    printf("%ld %ld %ld\n", ia, ic, id);
                     continue;
-                } else if(ia == ib && ib == ic && ic != id && a[ia] >= 3)
+                } else if(ia == ib && ib == ic && ic != id && a[ia] >= 3 && !us[hsh(ia, ib, ic, id)])
                 {
                     ans += (a[ia]*(a[ia] - 1)*(a[ia] - 2)) / fac(3) * a[id];
+                    us[hsh(ia, ib, ic, id)] = 42;
 //                    printf("%ld %ld\n", ia, id);
                     continue;
-                } else if(a[ia] >= 4)
+                } else if(a[ia] >= 4 && !us[hsh(ia, ib, ic, id)])
                 {
                     ans += (a[ia]*(a[ia] - 1) * (a[ia] - 2) * (a[ia] - 3)) / fac(4);
+                    us[hsh(ia, ib, ic, id)] = 42;
 //                    printf("%ld\n", ia);
                     continue;
                 }
             }
         }
     }
-    cout << (ans / 5) << endl;
+    cout << ans << endl;
     return 0;
 }
