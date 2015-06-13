0d4b2c854413e2d0be6d2119d1be9091aa3358e2
Jerry Sewell <ellesterate@gmail.com>
2015-06-13 20:03:56 +0300

    Looks better

--- cfrating/main.py
+++ cfrating/main.py
@@ -8,23 +8,16 @@ os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 1, 
 r = requests.get('http://codeforces.com/api/user.info', params={'handles':'Jyree'})
 
 s = r.text
-p1 = s.find('"rating"')
-p2 = s.find('"maxRank"')
-s = s[p1:p2-1]
-p1 = s.find(':')
-s = int(s[p1+1:])
+s = s[s.find('"rating"'):s.find('"maxRank"')-1]
+s = int(s[s.find(':')+1:])
 
 print(s)
 
 while True:
 	r = requests.get('http://codeforces.com/api/user.info', params={'handles':'Jyree'})
-	
-	d = r.text
-	p1 = d.find('"rating"')
-	p2 = d.find('"maxRank"')
-	d = d[p1:p2-1]
-	p1 = d.find(':')
-	d = int(d[p1+1:])
+
+	d = r.text[r.text.find('"rating"'):r.text.find('"maxRank"')-1]
+	d = int(d[d.find(':')+1:])
 
 	if s != d:
 		print(d)
