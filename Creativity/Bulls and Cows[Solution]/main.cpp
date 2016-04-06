bc4b87b5933e8758f37f37b969fa928fa3e59c12
Jerry Sewell <ellesterate@gmail.com>
2016-04-06 13:54:54 +0300

    Refactored

--- Creativity/Bulls and Cows[Solution]/main.cpp
+++ Creativity/Bulls and Cows[Solution]/main.cpp
@@ -1,52 +1,12 @@
-/*
-        ♥♥♥♥♥♥───♥♥♥♥───♥♥♥♥♥───♥♥──♥♥───♥♥♥♥───♥♥──♥♥───♥♥♥♥─────♥♥♥
-        ♥♥──────♥♥──♥♥──♥♥──♥♥──♥♥──♥♥──♥♥──♥♥──♥♥─♥♥───♥♥──♥♥───♥♥─♥♥
-        ♥♥──────♥♥──♥♥──♥♥♥♥♥───♥♥♥♥♥♥──♥♥──♥♥──♥♥♥♥────♥♥──♥♥───♥♥─♥♥
-        ♥♥──────♥♥──♥♥──♥♥──♥♥──♥♥──♥♥──♥♥──♥♥──♥♥─♥♥───♥♥──♥♥──♥♥♥♥♥♥♥
-        ♥♥───────♥♥♥♥───♥♥♥♥♥───♥♥──♥♥───♥♥♥♥───♥♥──♥♥───♥♥♥♥───♥─────♥
-*/
-
 #include <bits/stdc++.h>
 
-#define F first
-#define S second
-#define pb push_back
-#define mp make_pair
-#define pob pop_back
-
-#define GCD __gcd
-
 using namespace std;
-
-#ifdef LOCAL
-#define TASK "task"
-#define debug(...) printf(__VA_ARGS__)
-#else
-#define TASK "diamond"
-#define debug(...)
-#define cerr if(0)cerr
-#endif // LOCAL
-//                R  U  L   D
-const int sy[] = {0, 1, 0, -1};
-const int sx[] = {1, 0, -1, 0};
 
 int cows = 0, bulls = 0;
 string resp;
 set<int> s;
 
-void upd()
-{
-    cows += resp[1] - '0';
-    bulls += resp[0] - '0';
-}
-
-void check()
-{
-    if (resp == "40")
-        exit(0);
-}
-
-string itos(int x)
+string itos(int x)  // int to string
 {
     string res;
     while (x)
@@ -57,16 +17,20 @@ string itos(int x)
     return res;
 }
 
-string zeros(int x)
+string zeros(int x) // Transform int to 4-char str
 {
     string res;
     int nos = 0, y = x;
     while (y)
-        y /= 10,
+    {
+        y /= 10;
         nos++;
+    }
     while (nos < 4)
-        res += "0",
+    {
+        res += "0";
         nos++;
+    }
     res += itos(x);
     return res;
 }
@@ -75,37 +39,42 @@ bool check(int x, int mock)
 {
     int b = 0;
     string _x = zeros(x), _mock = zeros(mock);
-//    cerr << _x << " " << _mock << endl;
+    int c = 0;
+    vector<int> nm(10, 0);
+
     for (int i = 0; i < 4; ++i)
         if (_x[i] == _mock[i])
             b++;
-    int c = 0;
-    vector<int> nm(10, 0);
+
     for (int i = 0; i < 4; ++i)
         nm[(int) _mock[i] - '0']++;
+
     for (int i = 0; i < 4; ++i)
         if (nm[(int) _x[i] - '0'] > 0)
             c++;
+
     c -= b;
-    if (b != (int) resp[0] - '0' || c != (int) resp[1] - '0')
-        return true;
-    else
-        return false;
-//    cerr << b << " " << c << endl;
+
+    return (b != (int) resp[0] - '0' || c != (int) resp[1] - '0');
 }
 
 bool can(int x)
 {
     vector<int> nm(10, 0);
     int nos = 0, y = x;
-    while (x)
-        nm[x % 10]++,
-        x /= 10,
-        nos++;
     bool res = true;
+
+    while (x)
+    {
+        nm[x % 10]++;
+        x /= 10;
+        nos++;
+    }
+
     for (int i = 0; i < 10; ++i)
         if (nm[i] > 1)
             res = false;
+
     if (nos == 3)
         while (y)
         {
@@ -113,39 +82,40 @@ bool can(int x)
                 res = false;
             y /= 10;
         }
+
     return res;
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
-#ifdef LOCAL
-//	freopen(TASK ".in", "r", stdin);
-//	freopen(TASK ".out", "w", stdout);
-#endif // LOCAL
+
+    set<int> used;
+
     for (int i = 123; i < 10000; ++i)
         if (can(i))
             s.insert(i);
-//    check(123, 5123);
-//    return 0;
 
-    set<int> used;
     while (resp != "40")
     {
+        vector<int> todelete;
         int cur = -1;
+
         for (auto i = s.begin(); i != s.end() && cur == -1; ++i)
             if (used.count(*i) == 0)
                 cur = *i;
-//        int cur = *s.begin();
+
         cout << zeros(cur) << endl;
         cin >> resp;
         used.insert(cur);
+
         if (resp == "40")
             return 0;
-        vector<int> todelete;
-        for (set<int>::iterator i = s.begin(); i != s.end(); ++i)
+
+        for (auto i = s.begin(); i != s.end(); ++i)
             if (check(cur, *i))
-                todelete.pb(*i);
+                todelete.push_back(*i);
+
         for (int i = 0; i < todelete.size(); ++i)
             s.erase(todelete[i]);
     }
